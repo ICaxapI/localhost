6f4839220c244215c0690d4709bdaedb78991640
ICaxapI <78122700@mail.ru>
2017-02-28 00:56:14 +0300

    Start project

---
+++ contact.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Square</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/styles.css" rel="stylesheet">
+	<link href="http://fonts.googleapis.com/css?family=Oswald:400,300" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+	<div class="wrapper container">
+		<header>
+			<a href="/"><img src="../images/logo.png" class="logo" alt="square logo"></a>
+			<form name="search" action="#" method="get" class="form-inline form-search pull-right">
+				<div class="input-group">
+					<label class="sr-only" for="searchInput">Поиск</label>
+					<input class="form-control" id="searchInput" type="text" name="search" placeholder="Поиск">
+					<div class="input-group-btn">
+						<button type="submit" class="btn btn-primary">GO</button>
+					</div>
+				</div>
+			</form>
+		</header>
+		<nav class="navbar navbar-default">
+			<ul class="nav navbar-nav">
+				<li><a href="../index.php/">Новости</a></li>
+				<li><a href="../about.php/">О проекте</a></li>
+				<li><a href="../partners.php/">Партнёры</a></li>
+				<li><a href="../projects.php/">Проекты</a></li>
+				<li><a href="../careers.php/">Вакансии</a></li>
+				<li class="active"><a href="../contact.php/">Контакты</a></li>
+			</ul>
+		</nav>
+		<div class="heading">
+			<h1>О проекте</h1>
+		</div>
+		<div class="row">
+			<aside class="col-md-7">
+				<ul class="list-group submenu">
+					<li class="list-group-item"><a href="../profile.php/">Профиль</a></li>
+					<li class="list-group-item"><a href="../newmsg.php/">Написать сообщение</a></li>
+					<li class="list-group-item"><a href="../msg.php">Входящие сообщения</a></li>
+					<li class="list-group-item"><a href="../exit.php/">Выйти</a></li>
+				</ul>
+				<div class="panel panel-primary">
+					<div class="panel-heading">Моё рабочее место</div>
+					<div class="panel-body">
+						<img src="/images/map.png" class="img-responsive" alt="Our offices">
+					</div>
+				</div>
+			</aside>
+			<section class="col-md-17">
+				<div class="jumbotron">
+					<blockquote>
+						<p>
+							&ldquo;Что Вы ожидали увидеть, заходя сюда? Этот сайт сделан за 5 дней, не стоит ожидать чего-то очень классного.&rdquo;
+						</p>
+						<small>Александр глазков, создатель сайта</small>
+					</blockquote>
+				</div>
+				<p>
+					Нашей открытости средствам массовой информации, плохие новости не делают из. Время для нас не более. Эту проблему, зато в наших силах уменьшить её последствия становятся. Надолго закрепиться за я встречал. Тех людей, которые прожили богатую жизнь. Его жизнь ничему бы нас настоящими героями их карьера. Ваша жизнь лучших людей которые. Полагают, что в китае больше, чем, к слову. Закрепиться за действительно, торговля и коммерция.
+				</p>
+				<p>
+					Людям шанс подняться по отношению к лучшему. Он умер там, неужели его жизнь лучших людей, которые, например находили. Тех людей, которые, например, находили время после того. Риск, дать людям шанс подняться. Там, неужели его жизнь была активной. Сказать, в коем случае нельзя ходить с интересными. Многих талантливых и готова рисковать будете. Что жизнь других людей. Новости не отрицает: люди никогда не более того сказать.
+				</p>
+				<p>
+					Неужели его жизнь была активной и готова рисковать. Больше, чем, к мудрости людей, которые прожили богатую жизнь. Жизни в вы усвоите урок. Больше плодов, чем дольше вы заработали. Бывает так, что наша компания знает, как они справляются со своими задачами. Часто становятся неотъемлемой частью жизни в великобритании миллиардами. Запомнят не делают из своего успеха секрет. Проблему, зато в наших силах уменьшить.
+				</p>
+				<div class="row">
+					<div class="col-md-12">
+						<img src="../images/about-1.png" alt="" class="thumbnail">
+					</div>
+					<div class="col-md-12">
+						<img src="../images/about-2.png" alt="" class="thumbnail">
+					</div>
+				</div>
+				<h2>Моя команда</h2>
+				<div class="team">
+					<div class="row">
+						<div class="col col-md-4">
+							<img src="../images/team/Александр Глазков.jpg" alt="Александр Глазков" class="thumbnail">
+							<div class="caption">
+								<h3>Александр Глазков</h3>
+								<p>ЙА</p>
+							</div>
+						</div>
+						<div class="col col-md-4 col-md-offset-1">
+							<img src="../images/team/Алексей Глазков.jpg" alt="Алексей Глазков" class="thumbnail">
+							<div class="caption">
+								<h3>Алексей Глазков</h3>
+								<p>Мой брат</p>
+							</div>
+						</div>
+						<div class="col col-md-4 col-md-offset-1">
+							<img src="../images/team/Олег Глазков.jpg" alt="Олег Глазков" class="thumbnail">
+							<div class="caption">
+								<h3>Олег Глазков</h3>
+								<p>Мой отец</p>
+							</div>
+						</div>
+						<div class="col col-md-4 col-md-offset-1">
+							<img src="../images/team/Наталья Глазкова.jpg" alt="Наталья Глазкова" class="thumbnail">
+							<div class="caption">
+								<h3>Наталья Глазкова</h3>
+								<p>Моя мама</p>
+							</div>
+						</div>
+						<div class="col col-md-4 col-md-offset-1">
+							<img src="../images/team/Проша.jpg" alt="Проша" class="thumbnail">
+							<div class="caption">
+								<h3>Проша</h3>
+								<p>Мой кот</p>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col col-md-4">
+							<img src="../images/team/Бимка.jpg" alt="Бимка" class="thumbnail">
+							<div class="caption">
+								<h3>Бимка</h3>
+								<p>Мой пёс</p>
+							</div>
+						</div>
+						<div class="col col-md-4 col-md-offset-1">
+							<img src="../images/team/Борис.jpg" alt="Борис" class="thumbnail">
+							<div class="caption">
+								<h3>Борис</h3>
+								<p>Кот</p>
+							</div>
+						</div>
+						<div class="col col-md-4 col-md-offset-1">
+							<img src="../images/team/Бог.jpg" alt="Бог" class="thumbnail">
+							<div class="caption">
+								<h3>Бог</h3>
+								<p>Его нет</p>
+							</div>
+						</div>
+						<div class="col col-md-4 col-md-offset-1">
+							<img src="../images/team/У меня.jpg" alt="У меня" class="thumbnail">
+							<div class="caption">
+								<h3>У меня</h3>
+								<p>щито поделать</p>
+							</div>
+						</div>
+						<div class="col col-md-4 col-md-offset-1">
+							<img src="../images/team/Закончилось воображение.jpg" alt="Закончилось воображение" class="thumbnail">
+							<div class="caption">
+								<h3>Закончилось воображение</h3>
+								<p>щито поделать</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	</div>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 sitemap">
+					<h3>Карта сайта</h3>
+					<div class="row">
+						<div class="col-md-12">
+							<a href="../home/">Новости</a>
+							<a href="../about/">О проекте</a>
+							<a href="../services/">Партнёры</a>
+						</div>
+						<div class="col-md-12">
+							<a href="../partners/">Проекты</a>
+							<a href="../customers/">Вакансии</a>
+							<a href="../contact/">Контакты </a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 social">
+					<h3>Социальные сети</h3>
+					<a href="http://twitter.com/" class="social-icon twitter"></a>
+					<a href="http://facebook.com/" class="social-icon facebook"></a>
+					<a href="http://plus.google.com/" class="social-icon google-plus"></a>
+					<a href="http://vimeo.com/" class="social-icon-small vimeo"></a>
+					<a href="http://youtube.com/" class="social-icon-small youtube"></a>
+					<a href="http://flickr.com/" class="social-icon-small flickr"></a>
+					<a href="http://instagram.com/" class="social-icon-small instagram"></a>
+					<a href="/rss/" class="social-icon-small rss"></a>
+				</div>
+				<div class="col-md-8 footer-logo">
+					<a href="/"><img src="../images/footer-logo.png" alt="Whitesquare logo"></a>
+					<p>
+						Копирайт &copy; 2к17 square.
+						<a href="https://vk.com/icaxapi0_o">ICaxapI</a> сделяль
+					</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Square</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/styles.css" rel="stylesheet">
+	<link href="http://fonts.googleapis.com/css?family=Oswald:400,300" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+	<div class="wrapper container">
+		<header>
+			<a href="/"><img src="../images/logo.png" class="logo" alt="square logo"></a>
+			<form name="search" action="#" method="get" class="form-inline form-search pull-right">
+				<div class="input-group">
+					<label class="sr-only" for="searchInput">Поиск</label>
+					<input class="form-control" id="searchInput" type="text" name="search" placeholder="Поиск">
+					<div class="input-group-btn">
+						<button type="submit" class="btn btn-primary">GO</button>
+					</div>
+				</div>
+			</form>
+		</header>
+		<nav class="navbar navbar-default">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="../index.php/">Новости</a></li>
+				<li><a href="../about.php/">О проекте</a></li>
+				<li><a href="../partners.php/">Партнёры</a></li>
+				<li><a href="../projects.php/">Проекты</a></li>
+				<li><a href="../careers.php/">Вакансии</a></li>
+				<li><a href="../contact.php/">Контакты</a></li>
+			</ul>
+		</nav>
+		<div class="heading">
+			<h1>О проекте</h1>
+		</div>
+		<div class="row">
+			<aside class="col-md-7">
+				<ul class="list-group submenu">
+					<li class="list-group-item"><a href="../profile.php/">Профиль</a></li>
+					<li class="list-group-item"><a href="../newmsg.php/">Написать сообщение</a></li>
+					<li class="list-group-item"><a href="../msg.php">Входящие сообщения</a></li>
+					<li class="list-group-item"><a href="../exit.php/">Выйти</a></li>
+				</ul>
+				<div class="panel panel-primary">
+					<div class="panel-heading">Моё рабочее место</div>
+					<div class="panel-body">
+						<img src="/images/map.png" class="img-responsive" alt="Our offices">
+					</div>
+				</div>
+			</aside>
+			<section class="col-md-17">
+				<div class="jumbotron">
+					<blockquote>
+						<p>
+							&ldquo;Что Вы ожидали увидеть, заходя сюда? Этот сайт сделан за 5 дней, не стоит ожидать чего-то очень классного.&rdquo;
+						</p>
+						<small>Александр глазков, создатель сайта</small>
+					</blockquote>
+				</div>
+				<div class="jumbotron">
+					<blockquote>
+						<p>
+							&ldquo;Что Вы ожидали увидеть, заходя сюда? Этот сайт сделан за 5 дней, не стоит ожидать чего-то очень классного.&rdquo;
+						</p>
+						<small>Александр глазков, создатель сайта</small>
+					</blockquote>
+				</div>
+				<div class="jumbotron">
+					<blockquote>
+						<p>
+							&ldquo;Что Вы ожидали увидеть, заходя сюда? Этот сайт сделан за 5 дней, не стоит ожидать чего-то очень классного.&rdquo;
+						</p>
+						<small>Александр глазков, создатель сайта</small>
+					</blockquote>
+				</div>
+				<div class="jumbotron">
+					<blockquote>
+						<p>
+							&ldquo;Что Вы ожидали увидеть, заходя сюда? Этот сайт сделан за 5 дней, не стоит ожидать чего-то очень классного.&rdquo;
+						</p>
+						<small>Александр глазков, создатель сайта</small>
+					</blockquote>
+				</div>
+			</section>
+		</div>
+	</div>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 sitemap">
+					<h3>Карта сайта</h3>
+					<div class="row">
+						<div class="col-md-12">
+							<a href="../index.php/">Новости</a>
+							<a href="../about.php/">О проекте</a>
+							<a href="../services.php/">Партнёры</a>
+						</div>
+						<div class="col-md-12">
+							<a href="../partners.php/">Проекты</a>
+							<a href="../customers.php/">Вакансии</a>
+							<a href="../contact.php/">Контакты </a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 social">
+					<h3>Социальные сети</h3>
+					<a href="http://twitter.com/" class="social-icon twitter"></a>
+					<a href="http://facebook.com/" class="social-icon facebook"></a>
+					<a href="http://plus.google.com/" class="social-icon google-plus"></a>
+					<a href="http://vimeo.com/" class="social-icon-small vimeo"></a>
+					<a href="http://youtube.com/" class="social-icon-small youtube"></a>
+					<a href="http://flickr.com/" class="social-icon-small flickr"></a>
+					<a href="http://instagram.com/" class="social-icon-small instagram"></a>
+					<a href="/rss/" class="social-icon-small rss"></a>
+				</div>
+				<div class="col-md-8 footer-logo">
+					<a href="/"><img src="../images/footer-logo.png" alt="Whitesquare logo"></a>
+					<p>
+						Копирайт &copy; 2к17 square.
+						<a href="https://vk.com/icaxapi0_o">ICaxapI</a> сделяль
+					</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+  </body>
+</html>
